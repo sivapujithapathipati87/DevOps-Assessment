55377cfa19820a3852318dc3693507c6e70153c9
sivapujithapathipati87 <148363015+sivapujithapathipati87@users.noreply.github.com>
2024-08-19 19:15:02 +0530

    Update cluster.tf

---
+++ terraform/cluster.tf
@@ -0,0 +1,23 @@
+resource "aws_ecs_cluster" "notification_cluster" {
+  name = "notification-cluster"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
+}
